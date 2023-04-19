88a303ec1d1eadfcd7e39f3ff97d2246e9ad5159
David Garcia <davidgarciama@outlook.com>
2023-04-18 20:05:50 -0600

    MinLength added to the text area on the first modal

--- src/pages/TextForm/TextForm.js
+++ src/pages/TextForm/TextForm.js
@@ -4,30 +4,31 @@ import {LanguageContext} from "@/pages/LanguageProvider/LanguageProvider";
 
 function TextForm({text, setText, beforeModal, nextModal, label = null}) {
     const {t} = React.useContext(LanguageContext);
-    let minLength = () => {
-        if (text.length < 15) {
-            return `${t?.minLengthMessage}`;
+    let minLengthValidation = () => {
+        if(text){
+            if (text.length < 15) {
+
+                return `${t?.minLengthMessage}`;
+            } 
         }
+
         return ''
 
     }
 
     return (
         <form className={styles.textForm} action="" onSubmit={() => {
-            if (minLength() !== '') {
-                return
-            }
             beforeModal(false)
             nextModal(true)
         }}>
             {label && <label htmlFor='text-area'>{label}</label>}
-            <textarea minLength={15} maxLength={161} onInvalid={(event) => event.target.setCustomValidity(t?.required)}
+            <textarea  onInvalid={(event) => event.target.setCustomValidity(t?.required)}
                       onBlur={(event) => event.target.setCustomValidity('')} id={'text-area'} required={true}
                       value={text} onChange={(e) => {
                 setText(e.currentTarget.value)
             }} className={styles.textBox}></textarea>
-            <div>{minLength()}</div>
-            <button disabled={minLength() !== ''} type={"submit"}>{t?.textFormNextButton}
+            <div>{minLengthValidation()}</div>
+            <button disabled={minLengthValidation() !== ''} type={"submit"}>{t?.textFormNextButton}
             </button>
         </form>
     )
