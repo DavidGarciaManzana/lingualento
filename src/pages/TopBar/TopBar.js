fa1790cb747a5d8a212c9b99f7a6742c11d8a4f9
David Garcia <davidgarciama@outlook.com>
2023-04-20 20:22:11 -0600

    Desktop css changed

--- src/pages/TopBar/TopBar.js
+++ src/pages/TopBar/TopBar.js
@@ -2,16 +2,20 @@ import React from 'react'
 import styles from '@/pages/TopBar/TopBar.module.css'
 import {HelpCircle, Twitter} from "react-feather";
 import Button from "@/pages/Button/Button";
+import {useMediaQuery} from 'react-responsive'
 function TopBar({toggleInitialModal}) {
+    const isDesktop = useMediaQuery({ minWidth: 767 });
     return (
         <>
             <div className={styles.topBar}>
                 <Button className={styles.modalButton} onClick={()=>{toggleInitialModal(true)}}>
-                    <HelpCircle></HelpCircle>
+                    {isDesktop ? (<HelpCircle strokeWidth={1} width={'100%'} height='100%'/>):<HelpCircle/>}
                 </Button>
-                <h1>Lingualento</h1>
+                <h1 className={styles.title}>Lingualento</h1>
                 <Button className={styles.twitterButton}>
-                    <a href='https://twitter.com/DavidGarciaMa1' target="_blank"><Twitter></Twitter></a>
+                    <a className={styles.link} href='https://twitter.com/DavidGarciaMa1' target="_blank">
+                        {isDesktop ? (<Twitter strokeWidth={1} width={'100%'} height='100%'/>):<Twitter/>}
+                    </a>
                 </Button>
             </div>
         </>
